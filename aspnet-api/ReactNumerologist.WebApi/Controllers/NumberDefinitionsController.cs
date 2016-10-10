57b45c68e346367ad9cea48510b07ef45f770dfd
Michał Wilczyński <michal.e.wilczynski@gmail.com>
2016-10-10 19:57:41 +0200

    Preparing proper controllers for fetching number related data from birthdate and fullname

---
+++ aspnet-api/ReactNumerologist.WebApi/Controllers/NumberDefinitionsController.cs
@@ -0,0 +1,33 @@
+﻿namespace ReactNumerologist.WebApi.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using System;
+    using Definitions;
+    using Providers;
+
+    [Route("api/definitions")]
+    public class NumberDefinitionsController : Controller
+    {
+        [HttpGet("byBirthdate/{date}")]
+        public NumberDefinition GetDefinitionByBirthDate(DateTime date)
+        {
+            var number = new NumberFromDateProvider().ProvideNumber(date);
+            return new NumberDefinitionProvider().ProvideFor(number);
+        }
+
+        [HttpGet("byFullname/{fullname}")]
+        public NumberDefinition GetDefinitionByFullName(string fullname)
+        {
+            var number = new NumberFromFullNameProvider().ProvideNumber(fullname);
+            return new NumberDefinitionProvider().ProvideFor(number);
+        }
+
+        [HttpGet("byNumber/{number}")]
+        public NumberDefinition GetDefinitionByNumber(int number)
+        {
+            if (number < 1 || number > 9)
+                throw new ArgumentOutOfRangeException();
+            return new NumberDefinitionProvider().ProvideFor(number);
+        }
+    }
+}
---
+++ aspnet-api/ReactNumerologist.WebApi/Controllers/NumbersController.cs
@@ -0,0 +1,24 @@
+﻿namespace ReactNumerologist.WebApi.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using System;
+    using Definitions;
+    using Providers;
+
+    [Route("api/numbers")]
+    public class NumbersController : Controller
+    {
+        [HttpGet("byBirthdate/{date}")]
+        public int GetNumberByBirthDate(DateTime date)
+        {
+            return new NumberFromDateProvider().ProvideNumber(date);
+        }
+
+        [HttpGet("byFullname/{fullname}")]
+        public int GetNumberByFullName(string fullname)
+        {
+            return new NumberFromFullNameProvider().ProvideNumber(fullname);
+        }
+
+    }
+}
